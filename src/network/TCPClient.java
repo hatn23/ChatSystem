85c03f21bb2a1cf8f46f48d025b17301fd7af055
hatn23 <nguyet.ha.tran23@gmail.com>
2019-12-04 20:26:52 +0100

    Comment User and Message to test

--- src/network/TCPClient.java
+++ src/network/TCPClient.java
@@ -3,29 +3,33 @@ package network;
 import java.net.*;
 import java.util.*;
 import java.io.*;
-import data.User;
-import data.Message;
+//import data.User;
+//import data.Message;
 public class TCPClient {
 	
-	private static Socket socket;
-	private static User user;
-	private static Message message;
+	private static Socket clientSocket;
+	//private static User user;
+	//private static Message message;
 	private static String serverIP;
 	private static PrintWriter output;
 	private static BufferedReader input;
 	private static Scanner scanner;
+	private static String msg;
 	
 	public static void main(String[] args) throws IOException {
-		socket = new Socket(InetAddress.getByName(serverIP), user.getPort());
-		output = new PrintWriter(socket.getOutputStream());
-		input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		//clientSocket = new Socket(InetAddress.getByName(serverIP), user.getPort());
+		clientSocket = new Socket("127.0.0.1",4000);
+		output = new PrintWriter(clientSocket.getOutputStream());
+		input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 		scanner = new Scanner(System.in);
 		Thread send = new Thread(new Runnable() {
 			public void run() {
 				while(true) {
-					message.setSender(user.getPseudo());
+					/*message.setSender(user.getPseudo());
 					message.setMessage(scanner.nextLine());
-					output.println(message.getMessage());
+					output.println(message.getMessage());*/
+					msg = scanner.nextLine();
+	                output.println(msg);
 					output.flush();
 				}
 			}
@@ -36,14 +40,19 @@ public class TCPClient {
 			public void run() {
 				while(true) {
 					try {
-						message.setMessage(input.readLine());
+						/*message.setMessage(input.readLine());
 						while (message.getMessage()!= null){
 							System.out.println(message.getSender() + " : " + message.getMessage());
-							message.setMessage(input.readLine());
-						}
+							message.setMessage(input.readLine());*/
+						msg = input.readLine();
+		                while(msg!=null){
+		                	System.out.println("Serveur : "+msg);
+		                	msg = input.readLine();
+		                 }
+						
 						System.out.println("Server disconnected");
 						output.close();
-						socket.close();
+						clientSocket.close();
 					} catch (IOException e) {
 						e.printStackTrace();
 					}
--- src/network/TCPServer.java
+++ src/network/TCPServer.java
@@ -2,23 +2,25 @@ package network;
 import java.net.*;
 import java.util.*;
 import java.io.*;
-import data.Message;
-import data.User;
+//import data.Message;
+//import data.User;
 
 public class TCPServer {
 	
 	private static ServerSocket serverSocket;
 	private static Socket clientSocket;
-	private static User user;
-	private static Message message;
+	//private static User user;
+	//private static Message message;
 	private static String serverIP;
 	private static PrintWriter output;
 	private static BufferedReader input;
 	private static Scanner scanner;
+	private static String msg;
 	
 
 	public static void main(String[] args) throws IOException {
-		serverSocket = new ServerSocket(user.getPort());
+		//serverSocket = new ServerSocket(user.getPort());
+		serverSocket = new ServerSocket(4000);
 		clientSocket = serverSocket.accept();
 		output = new PrintWriter(clientSocket.getOutputStream());
 		input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
@@ -28,8 +30,10 @@ public class TCPServer {
 			public void run() {
 				while(true) {
 					/*message.setSender(user.getPseudo());*/
-					message.setMessage(scanner.nextLine());
-					output.println(message.getMessage());
+					//message.setMessage(scanner.nextLine());
+					//output.println(message.getMessage());
+					msg = scanner.nextLine();
+	                output.println(msg);
 					output.flush();
 					}
 				}
@@ -40,12 +44,17 @@ public class TCPServer {
 			public void run() {
 				while (true) {
 					try {
-						message.setMessage(input.readLine());
+						//message.setMessage(input.readLine());
+						msg = input.readLine();
 						//while user is still connected
-						while (message.getMessage()!= null){
+						/*while (message.getMessage()!= null){
 							System.out.println(message.getSender() + " : " + message.getMessage());
 							message.setMessage(input.readLine());
-						}
+						}*/
+						while(msg!=null){
+			                   System.out.println("Client : "+msg);
+			                   msg = input.readLine();
+			                }
 						System.out.println("Client is disconnected");
 						output.close();
 						clientSocket.close();
@@ -60,4 +69,6 @@ public class TCPServer {
 		receive.start();
 		
 	}
+	
+
 }
